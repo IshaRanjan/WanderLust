c977210efa1600e34f58f3f4e755b46d4ecd0f95
IshaRanjan <147339092+IshaRanjan@users.noreply.github.com>
2026-08-05 00:17:09 +0530

    Initial commit

---
+++ models/listing.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./reviews.js");
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description:{
+    type: String,
+  },
+  image: {
+    type: String,
+    set:(v)=>
+      v===""
+      ?"https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      :v,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  reviews:[
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    }
+  ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing)=>{
+  if(listing){
+    await Review.deleteMany({_id:{ $in:listing.reviews}
+  });
+  }
+});
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
